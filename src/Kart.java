ff1e632b6ee489297901c3953eb07ee36e436250
githsem <53379599+githsem@users.noreply.github.com>
2020-08-12 13:44:19 +0200

    Initial commit

---
+++ src/Kart.java
@@ -0,0 +1,28 @@
+
+public class Kart {
+    
+    private char deger;
+    private boolean tahmin = false;
+
+    public char getDeger() {
+        return deger;
+    }
+
+    public void setDeger(char deger) {
+        this.deger = deger;
+    }
+
+    public boolean isTahmin() {
+        return tahmin;
+    }
+
+    public void setTahmin(boolean tahmin) {
+        this.tahmin = tahmin;
+    }
+
+    public Kart(char deger) {
+        this.deger = deger;
+    }
+    
+    
+}
